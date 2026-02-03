f17f739b30b8bc1b017930e7bee542f31d49b48e
Luis Avila <manuelavila2023@gmail.com>
2026-02-03 00:45:16 -0600

    Load dotenv only in local environment

--- src/config/env.js
+++ src/config/env.js
@@ -1,7 +1,7 @@
-// src/config/env.js
+import dotenv from 'dotenv';
+
 // Cargar .env SOLO en local
 if (process.env.NODE_ENV !== 'production') {
-  const dotenv = await import('dotenv');
   dotenv.config();
 }
 
@@ -17,7 +17,12 @@ export const env = {
   JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN || '1d',
 };
 
-// process.loadEnvFile();
+// // src/config/env.js
+// // Cargar .env SOLO en local
+// if (process.env.NODE_ENV !== 'production') {
+//   const dotenv = await import('dotenv');
+//   dotenv.config();
+// }
 
 // const ENV = process.env.ENV || process.env.NODE_ENV || 'development';
 
@@ -30,3 +35,17 @@ export const env = {
 //   JWT_SECRET: process.env.JWT_SECRET,
 //   JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN || '1d',
 // };
+
+// // process.loadEnvFile();
+
+// // const ENV = process.env.ENV || process.env.NODE_ENV || 'development';
+
+// // export const env = {
+// //   ENV,
+// //   PORT: process.env.PORT || 3000,
+// //   DATABASE_URL: process.env.DATABASE_URL || '',
+// //   GOOGLE_APP_EMAIL: process.env.GOOGLE_APP_EMAIL || '',
+// //   GOOGLE_APP_PASSWORD: process.env.GOOGLE_APP_PASSWORD || '',
+// //   JWT_SECRET: process.env.JWT_SECRET,
+// //   JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN || '1d',
+// // };
