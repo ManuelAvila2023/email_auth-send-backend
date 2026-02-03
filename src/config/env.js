ee07ca4873dd01d5afc9e01588d17c2e28c606a3
Luis Avila <manuelavila2023@gmail.com>
2026-02-03 00:39:43 -0600

    Load dotenv only in local environment

--- src/config/env.js
+++ src/config/env.js
@@ -1,4 +1,9 @@
-process.loadEnvFile();
+// src/config/env.js
+// Cargar .env SOLO en local
+if (process.env.NODE_ENV !== 'production') {
+  const dotenv = await import('dotenv');
+  dotenv.config();
+}
 
 const ENV = process.env.ENV || process.env.NODE_ENV || 'development';
 
@@ -11,3 +16,17 @@ export const env = {
   JWT_SECRET: process.env.JWT_SECRET,
   JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN || '1d',
 };
+
+// process.loadEnvFile();
+
+// const ENV = process.env.ENV || process.env.NODE_ENV || 'development';
+
+// export const env = {
+//   ENV,
+//   PORT: process.env.PORT || 3000,
+//   DATABASE_URL: process.env.DATABASE_URL || '',
+//   GOOGLE_APP_EMAIL: process.env.GOOGLE_APP_EMAIL || '',
+//   GOOGLE_APP_PASSWORD: process.env.GOOGLE_APP_PASSWORD || '',
+//   JWT_SECRET: process.env.JWT_SECRET,
+//   JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN || '1d',
+// };
